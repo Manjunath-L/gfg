f3761906bd1bd0f6f8862ff8aac322a48d4b4917
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-08-16 23:10:18 +0530

    python

--- ml-gfg/day1.py
+++ ml-gfg/day1.py
@@ -16,3 +16,16 @@ print("p =",p," q =",q)
 print("if p is AND with q, it will output", p and q)  
 print("if p is OR with q, it will output",p or q)   
 print("not of p will output",not p)
+
+
+# Creating a String 
+String1 = "GeeksForGeeks"
+
+# Printing 3rd to 12th character 
+print("Slicing characters from 5-12: ") 
+print(String1[5:12]) 
+
+# Printing between 3rd and 2nd last character 
+print("Slicing characters between " +
+	"3rd and 3rd last character: ") 
+print(String1[3:-3])
