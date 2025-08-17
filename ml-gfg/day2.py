791ffe635182d82642f5127c8943c5526ebd5b6b
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-08-17 23:36:01 +0530

    day-2

---
+++ ml-gfg/day2.py
@@ -0,0 +1,23 @@
+# Implicit typecasting from int to string
+integer_number = 10
+string_number = "5"
+result = integer_number + string_number
+print(result)  # Output: TypeError (unsupported operand type(s) for +)
+
+# Implicit typecasting from int to string
+integer_number = 10
+string_number = "5"
+result = integer_number + string_number
+print(result)  # Output: TypeError (unsupported operand type(s) for +)
+
+# Creating a String 
+String1 = "GeeksForGeeks"
+
+# Printing 3rd to 12th character 
+print("Slicing characters from 5-12: ") 
+print(String1[5:12]) 
+
+# Printing between 3rd and 2nd last character 
+print("Slicing characters between " +
+	"3rd and 3rd last character: ") 
+print(String1[3:-3])
