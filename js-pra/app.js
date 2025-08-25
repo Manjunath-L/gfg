de2e7797682c11bdcb7f0ac93e04e2f18b72ce34
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-08-25 23:52:46 +0530

    js-project-change

--- js-pra/app.js
+++ js-pra/app.js
@@ -2,9 +2,15 @@ let prompt = require('prompt-sync')();
 
 let user_amswer = prompt("Enter a my favorite hero :");
 let ans = "D boss";
+let score = 10
+
+
 
 if (user_amswer === ans) {
     console.log("Your Write 👍");
+    console.log("Your current score :", score);
 } else {
     console.log("Your wrong ⛔");
+    score--
+    console.log("Your current score :", score);
 }
