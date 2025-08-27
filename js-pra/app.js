bc809169f0e59038ff053535b959d023abf387ec
Manju-2004 <radhamanjuradhamanju83@gmail.com>
2025-08-27 21:23:22 +0530

    guess-number

--- js-pra/app.js
+++ js-pra/app.js
@@ -1,28 +1,33 @@
-let prompt = require('prompt-sync')();
+// let prompt = require('prompt-sync')();
 
-let user_amswer = prompt("Enter a my favorite hero :");
-let ans = "D boss";
-let score = 10
+// let user_amswer = prompt("Enter a my favorite hero :");
+// let ans = "D boss";
+// let score = 10
 
 
 
-if (user_amswer === ans) {
-    console.log("Your Write 👍");
-    console.log("Your current score :", score);
-} else {
-    console.log("Your wrong ⛔");
-    score--
-    console.log("Your current score :", score);
-}
+// if (user_amswer === ans) {
+//     console.log("Your Write 👍");
+//     console.log("Your current score :", score);
+// } else {
+//     console.log("Your wrong ⛔");
+//     score--
+//     console.log("Your current score :", score);
+// }
 
-let animal = prompt("Enter a my favorite animal :");
-let animal_ans = "Elephant";
+// let animal = prompt("Enter a my favorite animal :");
+// let animal_ans = "Elephant";
 
-if (animal === animal_ans) {
-    console.log("Your Write 👍");
-    console.log("Your current score :", score);
-} else {
-    console.log("Your wrong ⛔");
-    score--
-    console.log("Your current score :", score);
-}
+// if (animal === animal_ans) {
+//     console.log("Your Write 👍");
+//     console.log("Your current score :", score);
+// } else {
+//     console.log("Your wrong ⛔");
+//     score--
+//     console.log("Your current score :", score);
+// }
+
+
+// "use strict";
+// undeclared = 20;
+// console.log(undeclared);
